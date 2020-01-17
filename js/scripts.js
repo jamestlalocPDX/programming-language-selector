426dc3b2ce2e2fe458a1c02ba3a728bba5e0e71b
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-01-17 14:01:02 -0800

    added button input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+var JavaScript = 
+
+
+
+
+
 $(document).ready(function () {
 
   $('input[name="music"]').on('change', (function(e){
@@ -15,9 +21,85 @@ $(document).ready(function () {
       return $(".movies").toggle();
     }
 
-    event.preventDefault();
-
   }));
+
+  $('input[name="movies"]').on('change', (function(e){
+    var movies = $("input:radio[name=movies]:checked").val();
+
+    if (movies === "adventure") {
+      return $(".weather").toggle();
+    } else if (movies === "horror") {
+      return $(".weather").toggle();
+    } else if (movies === "comedies") {
+      return $(".weather").toggle();
+    } else if (movies === "documentaries") {
+      return $(".weather").toggle();
+    } else {
+      return $(".weather").toggle();
+    }
+
+   
+  }));
+
+  $('input[name="weather"]').on('change', (function(e){
+    var weather = $("input:radio[name=weather]:checked").val();
+
+    if (weather === "sunny") {
+      return $(".dogs").toggle();
+    } else if (weather === "rainy") {
+      return $(".dogs").toggle();
+    } else if (weather === "stormy") {
+      return $(".dogs").toggle();
+    } else if (weather === "snowy") {
+      return $(".dogs").toggle();
+    } else {
+      return $(".dogs").toggle();
+    }
+
+    event.preventDefault();
+  }));
+
+  $('input[name="dogs"]').on('change', (function(e){
+    var dogs = $("input:radio[name=dogs]:checked").val();
+
+    if (dogs === "goldenretrievers") {
+      return $(".activities").toggle();
+    } else if (dogs === "corgis") {
+      return $(".activities").toggle();
+    } else if (dogs === "pitbulls") {
+      return $(".activities").toggle();
+    } else if (dogs === "huskies") {
+      return $(".activities").toggle();
+    } else {
+      return $(".activities").toggle();
+    }
+
+    
+  }));
+
+  $('input[name="activities"]').on('change', (function(e){
+    var activities = $("input:radio[name=activities]:checked").val();
+
+    if (activities === "inbed") {
+      $(".btn").toggle();
+    } else if (activities === "onahike") {
+      $(".btn").toggle();
+    } else if (activities === "bar") {
+      $(".btn").toggle();
+    } else if (activities === "hometown") {
+      $(".btn").toggle();
+    } else {
+      $(".btn").toggle();
+    }
+
+    
+  }));
+
+  //$(".btn").onClick(function() {
+   // var JavaScript = 
+    //var result = JavaScript
+    //$("#output").text(result)
+ // }))
 
 
 });
