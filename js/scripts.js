a35e275436967b3a2bf02babf7fe7af02a80e707
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-01-20 23:17:05 -0800

    conver indentations to spaces.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
-	$("form#questionaire").submit(function(event) {
-  	var music = parseInt($("input:radio[name=music]:checked").val());
-  	var movies = parseInt($("input:radio[name=movies]:checked").val());
-  	var weather = parseInt($("input:radio[name=weather]:checked").val());
-  	var dogs = parseInt($("input:radio[name=dogs]:checked").val());
-		var activities = parseInt($("input:radio[name=activities]:checked").val());
-		var result = (music + movies + weather + dogs + activities)
+  $("form#questionaire").submit(function(event) {
+    var music = parseInt($("input:radio[name=music]:checked").val());
+    var movies = parseInt($("input:radio[name=movies]:checked").val());
+    var weather = parseInt($("input:radio[name=weather]:checked").val());
+    var dogs = parseInt($("input:radio[name=dogs]:checked").val());
+    var activities = parseInt($("input:radio[name=activities]:checked").val());
+    var result = (music + movies + weather + dogs + activities)
 
-		if (result) {
-			if (result <= 63) {
-				$("p#javascript").show();
-			} else if (result > 63 && result < 68) {
-				$("p#ruby").show();
-			} else {
-				$("p#python").show();
-			}
-		} else {
-			alert("Oops! You missed one.");
-		}
+    if (result) {
+      if (result <= 63) {
+        $("p#javascript").show();
+      } else if (result > 63 && result < 68) {
+        $("p#ruby").show();
+      } else {
+        $("p#python").show();
+      }
+    } else {
+      alert("Oops! You missed one.");
+    }
 
-		event.preventDefault();
+    event.preventDefault();
    });
 });
