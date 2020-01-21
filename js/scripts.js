a048ab535b4d267fcc95defe5ea66868a55cdda5
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-01-20 18:54:21 -0800

    add a button to the end of the questional in html file, remove functions set for each individual questions, and replace those functione with one that creates a variable for each question name.

--- js/scripts.js
+++ js/scripts.js
@@ -1,104 +1,20 @@
 $(document).ready(function () {
+	$("form#questionaire").submit(function() {
+  	var music = $("input:radio[name=music]:checked").val();
+  	var movies = $("input:radio[name=movies]:checked").val();
+  	var weather = $("input:radio[name=weather]:checked").val();
+  	var dogs = $("input:radio[name=dogs]:checked").val();
+  	var activities =$("input:radio[name=activities]:checked").val();
 
-  $('input[name="music"]').on('change', (function(e){
-    var music = $("input:radio[name=music]:checked").val();
+  	if (music || weather || activities = rockandroll || sunny && bar) {
+    	.show("p#javascript");
+  	} else if ( movies || dogs = adventure && huskies) {
+			.show("p#ruby");
+		} else {
+			.show("python");
+		}
 
-    if (music === "rockandroll") {
-       $(".movies").toggle();
-    } else if (music === "jazz") {
-       $(".movies").toggle();
-    } else if (music === "rapandhiphop") {
-       $(".movies").toggle();
-    } else if (music === "metal") {
-       $(".movies").toggle();
-    } else {
-       $(".movies").toggle();
-    }
-
-    $(".music").toggle();
-
-    event.preventDefault();
-  }));
-
-  $('input[name="movies"]').on('change', (function(e){
-    var movies = $("input:radio[name=movies]:checked").val();
-
-    if (movies === "adventure") {
-       $(".weather").toggle();
-    } else if (movies === "horror") {
-       $(".weather").toggle();
-    } else if (movies === "comedies") {
-       $(".weather").toggle();
-    } else if (movies === "documentaries") {
-       $(".weather").toggle();
-    } else {
-       $(".weather").toggle();
-    }
-
-    $(".movies").toggle();
-
-    event.preventDefault();
-  }));
-
-  $('input[name="weather"]').on('change', (function(e){
-    var weather = $("input:radio[name=weather]:checked").val();
-
-    if (weather === "sunny") {
-       $(".dogs").toggle();
-    } else if (weather === "rainy") {
-       $(".dogs").toggle();
-    } else if (weather === "stormy") {
-       $(".dogs").toggle();
-    } else if (weather === "snowy") {
-       $(".dogs").toggle();
-    } else {
-       $(".dogs").toggle();
-    }
-
-    $(".weather").toggle();
-
-    event.preventDefault();
-  }));
-
-  $('input[name="dogs"]').on('change', (function(e){
-    var dogs = $("input:radio[name=dogs]:checked").val();
-
-    if (dogs === "goldenretrievers") {
-       $(".activities").toggle();
-    } else if (dogs === "corgis") {
-       $(".activities").toggle();
-    } else if (dogs === "pitbulls") {
-       $(".activities").toggle();
-    } else if (dogs === "huskies") {
-       $(".activities").toggle();
-    } else {
-       $(".activities").toggle();
-    }
-
-    $(".dogs").toggle();
-
-    event.preventDefault();
-  }));
-
-  $('input[name="activities"]').on('change', (function(e){
-    var activities = $("input:radio[name=activities]:checked").val();
-
-    if (activities === "inbed") {
-      
-    } else if (activities === "onahike") {
-      
-    } else if (activities === "bar") {
-      
-    } else if (activities === "hometown") {
-      
-    } else {
-      
-    }
-
-    
-
-    event.preventDefault();
-  }));
+   });
 
   
 
