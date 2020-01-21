bd6cdc8abda5479beecee6fe71b7edc605dffca7
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-01-20 19:19:12 -0800

    add branches to js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
 $(document).ready(function () {
-	$("form#questionaire").submit(function() {
+	$("form#questionaire").submit(function(event) {
   	var music = $("input:radio[name=music]:checked").val();
   	var movies = $("input:radio[name=movies]:checked").val();
   	var weather = $("input:radio[name=weather]:checked").val();
   	var dogs = $("input:radio[name=dogs]:checked").val();
   	var activities =$("input:radio[name=activities]:checked").val();
 
-  	if (music || weather || activities = rockandroll || sunny && bar) {
-    	.show("p#javascript");
-  	} else if ( movies || dogs = adventure && huskies) {
-			.show("p#ruby");
+  	if (movies || dogs === adventure && huskies) {
+    	$("p.javascript").show();
+  	} else if ( music || weather || activities === metal && sunny && atabar) {
+			$("p.ruby").show();
 		} else {
-			.show("python");
+			$("p.python").show();
 		}
 
+		event.preventDefault();
+
    });
-
-  
-
 });
